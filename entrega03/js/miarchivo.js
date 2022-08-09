c75ffe83e2bbc35bd24d1afd000e880c9afdc66f
alejandro <alitux.da@gmail.com>
2022-08-09 01:15:39 -0300

    clases y array entrega 3

--- entrega03/js/miarchivo.js
+++ entrega03/js/miarchivo.js
@@ -1,176 +1,223 @@
+//DATOS INICIALES
+const planesLista = [
+    {id:1,descripcion:"plan Basico" , multiplicador : 1},
+    {id:1,descripcion:"plan Medio", multiplicador : 2},
+    {id:1,descripcion:"plan full", multiplicador : 3},
+]
 
-
-class Seguros{
-    constructor(){
-        this.planes = `Seguros de Robo:
-        1) plan basico
-        2) plan medio
-        3) plan full
-        seleccione plan`;
-    
-        this.opciones = `Ingrese una opcion valida
-        1)registrar cliente
-        2)ofrecer planes
-        3)dar de baja plan
-        4)ver cliente y plan
-        5)salir`;
-        this.planSeleccionado = undefined;
-
+const clientesLista = [
+    {dni:35987001,nombre:"alejandro" , apellido : "galarza" ,edad:20},
+    {dni:35987002,nombre:"damian", apellido : "fernandez",edad:25},
+    {dni:35987003,nombre:"gabriela", apellido : "gomez", edad:30},
+]
+const polizaLista = [
+    {dniCliente:35987001,idPlan:1},
+    {dniCliente:35987001,idPlan:2},
+    {dniCliente:35987002,idPlan:3},
+    {dniCliente:35987003,idPlan:1},
+    {dniCliente:35987003,idPlan:3},
+]
+//CLASES
+class Plan{
+    constructor(id,descripcion,multiplicador){
+        this.baseSumaAsegurada = 1000;
+        this.basePremio = 0.1;
+        this.id = id;
+        this.descripcion = descripcion;
+        this.sumaAsegurada = this.baseSumaAsegurada * multiplicador;
+        this.premio = this.sumaAsegurada/this.basePremio;
+        
     }
-
-    detallePlan(multiplicador){
-        const sumaAsegurada = 1000*multiplicador;
-        const premio = sumaAsegurada*0.1;
-        const plan = `plan ${(multiplicador==1?"basico":multiplicador==2?"medio":"full")}
-                    * premio a pagar $${premio}
-                    * suma asegurada $${sumaAsegurada}`
-        return plan;
-    }
-
-    darDeBajaPlan(){
-        if(this.planSeleccionado != undefined){
-            this.planSeleccionado = undefined;
-            alert("se desvinculo el plan")
-        }else{
-            alert("el cliente no tiene seguro contratado")
-        }
-}
 }
 class Cliente{
-    constructor(){
-        this.nombre;
-        this.apellido;
-        this.documento;
-        this.edad;
+    constructor(dni,nombre,apellido,edad){
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
     }
 
-    registrarCliente(){
-        this.nombre = this.obtenerTextoValido("nombre");
-        this.apellido = this.obtenerTextoValido("apellido");
-        this.documento =  this.obtenerNumeroValido("documento");
-        this.edad = this.obtenerNumeroValido("edad");
-    }
-
-    obtenerNumeroValido  (descripcion){
-
-        let numero = parseInt( prompt(`${descripcion}:`).trim() )
-        while(isNaN(numero) || (!isNaN(numero) && numero <= 0 )){
-            numero = parseInt( prompt(`campo ${descripcion} es invalido, ingrese nuevamente :`) )
-        }
-        return numero;
-    }
-    
-    obtenerTextoValido (descripcion){
-    
-        let texto =  prompt(`${descripcion}:`).trim()
-        while(texto.length == 0){
-            texto =  prompt(`campo ${descripcion} es invalido, ingrese nuevamente :`) 
-        }
-        return texto;
+}
+class Poliza{
+    constructor(dniCliente,idPlan){
+        this.dniCliente = dniCliente;
+        this.idPlan = idPlan;        
     }
 }
-class Cotizador{
-   
+class Aseguradora{
     constructor(){
-
-        //datos del cliente 
-        this.cliente = new Cliente();
-        // plan seleccionado
-        this.seguros = new Seguros();
+        this.clientes = []
+        this.polizas = []
+        this.planes = []
+        
+        this.cargarClientes(clientesLista);
+        this.cargarPlanes(planesLista);
+        this.cargarPolizas(polizaLista);
     }
-
-
-    obtenerOpcionValida(){
-
-        let seleccion = 0;
-        do{
-            seleccion = parseInt( prompt(this.seguros.opciones))
-        }while(isNaN(seleccion)|| 0 >= seleccion || seleccion>5)
-    
-        return seleccion;
-    }
-
- 
-    comprarPlan(comprarPlanCodigo){
-        if(this.cliente.nombre != undefined && this.cliente.apellido != undefined){
-            if(this.seguros.planSeleccionado == undefined){
-                if(confirm("comprar el plan previamente visto ?")){
-                    this.seguros.planSeleccionado = comprarPlanCodigo;
-                    alert("se compro el plan :" + this.seguros.detallePlan(comprarPlanCodigo))
-                }else{
-                    alert("no se compro el plan");
-                }
-            }else{
-                alert("error, ya tienes un seguro")
+    cargarPlanes(lista){
+        for(const element of lista) {
+             this.planes.push(new Plan(element.id, element.descripcion, element.multiplicador));
+          }
+      }
+    cargarClientes(lista){
+        for(const element of lista) {
+                this.clientes.push(new Cliente(element.dni, element.nombre, element.apellido,element.edad));
             }
+        }
+
+    cargarPolizas(lista){
+        for(const element of lista) {
+                this.polizas.push(new Poliza(element.dniCliente, element.idPlan));
+            }
+        }
+    obtenerPlan(id){
+        return this.planes.find(plan=>plan.id==id);
+
+    }
+    ObtenerCliente(dni){
+        return this.clientes.find(cliente=>cliente.dni==dni);
+    }
+
+    ObtenerPoliza(dni,idPlan){
+        return this.polizas.find(poliza=>poliza.dniCliente==dni && poliza.idPlan==idPlan);
+    }
+
+    crearPoliza(cliente,plan){
+        if(this.ObtenerCliente(cliente.dni)!=undefined && this.obtenerPlan(plan.id)!=undefined){
+            this.polizas.push(new Poliza(cliente.dni, plan.id));
+            return true;
         }else{
-            alert("error, registre un cliente para comprar un plan")
+            return false;
         }
     }
-    
-    ofrecerPlanes(){
 
-        let seleccion = parseInt( prompt(this.seguros.planes))
-        switch (seleccion){
-             case 1:
-                 alert(this.seguros.detallePlan(1));
-                 this.comprarPlan(1)
-                 break;
-             case 2:
-                 alert(this.seguros.detallePlan(2))
-                 this.comprarPlan(2)
-                 break;
-             case 3:
-                 alert(this.seguros.detallePlan(3))
-                 this.comprarPlan(3)
-                 break;
-             default:
-                 alert("opcion incorrecta");
-                 break;   
-             }
-         }
-    
-
-    verClienteYPlan(){
-            let detalle="";
-            if(this.cliente.nombre != undefined && this.cliente.apellido != undefined){
-                detalle = `${this.cliente.nombre} ${this.cliente.apellido} `
-            }else{
-                detalle = `no hay cliente registrado`
-            }
-            if(this.seguros.planSeleccionado != undefined){
-                detalle = `${detalle}, ${this.seguros.detallePlan(this.seguros.planSeleccionado)}`
-            }else{
-                detalle = `${detalle}, no tiene plan contratado`
-            }
-            alert(detalle);
+    bajaPoliza(poliza){
+        this.polizas.splice(this.polizas.indexOf(poliza),1)
     }
-    operacion (){
+}
+
+class Programa{
+    constructor(){
+        this.aseguradora = new Aseguradora();
+        this.opcionesOperaciones = `Ingrese una opcion valida
+                        1)buscar cliente
+                        2)buscar planes
+                        3)buscar polizas
+                        4)crear cliente
+                        5)crear poliza
+                        6)baja poliza 
+                        7)salir`;
+        this.opcionesBuscarcliente = `Ingrese una opcion valida
+                        1)buscar cliente por dni
+                        2)listar a todos los clientes por consola`;
+        this.opcionesBuscarPlanes = `Ingrese una opcion valida
+                        1)buscar plan por id
+                        2)listar a todos los planes`;
+        this.opcionesBuscarPoliza = `Ingrese una opcion valida
+                        1)buscar poliza por dni
+                        2)buscar poliza por plan
+                        3)buscar poliza por plan y dni
+                        4)listar a todos los planes`;
+    
+    }
+
+    buscarCliente(){
+        let opcion = 0;
+        do{
+            opcion = parseInt( prompt(this.opcionesBuscarcliente))  
+            
+            if(opcion===1){
+                const dni = obtenerNumeroValido("dni");
+
+                const cliente = this.aseguradora.ObtenerCliente(dni);
+    
+                if(cliente != undefined){
+                    alert(`se encontro al cliente
+                            Nombre: ${cliente.nombre} apellido: ${cliente.apellido} dni: ${cliente.dni} edad: ${cliente.edad} `)
+                }else{
+                    alert("cliente no encontrado")
+                }
+     
+            }else{
+                for(const cliente of this.aseguradora.clientes){
+                    console.log(`Nombre: ${cliente.nombre} apellido: ${cliente.apellido} dni: ${cliente.dni} edad: ${cliente.edad} `);
+                }
+            }
+
+        }while(isNaN(opcion)|| 0 >= opcion || opcion>2)
+    }
+    opreaciones(){
+
         let opcion = 0;
         do {
-            opcion =  this.obtenerOpcionValida();
+            do{
+                opcion = parseInt( prompt(this.opcionesOperaciones))
+            }while(isNaN(opcion)|| 0 >= opcion || opcion>7)
+
             switch (opcion){
                 case 1:
-                    this.cliente = new Cliente();
-                    this.seguros = new Seguros();
-                    this.cliente.registrarCliente();
-                    
+                    this.buscarCliente();
                     break;
                 case 2:
-                    this.ofrecerPlanes();
+                     
                     break;
                 case 3:
-                    this.seguros.darDeBajaPlan();
+                     
                     break;                        
                 case 4:
-                    this.verClienteYPlan();
-                    break;   
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                                        
                 default:
                     break;   
                 }
-        } while (opcion !== 5 );
+        } while (opcion !== 7 );
+
+
+
     }
+
 }
 
-const cotizador = new Cotizador();
-cotizador.operacion();
+//funciones utiles
+const obtenerNumeroValido =  (descripcion)=>{
+
+    let numero = parseInt( prompt(`${descripcion}:`).trim() )
+    while(isNaN(numero) || (!isNaN(numero) && numero <= 0 )){
+        numero = parseInt( prompt(`campo ${descripcion} es invalido, ingrese nuevamente :`) )
+    }
+    return numero;
+}
+
+const obtenerTextoValido = (descripcion)=>{
+
+    let texto =  prompt(`${descripcion}:`).trim()
+    while(texto.length == 0){
+        texto =  prompt(`campo ${descripcion} es invalido, ingrese nuevamente :`) 
+    }
+    return texto;
+}
+
+ 
+ 
+
+const programa = new Programa();
+programa.opreaciones();
+
+     
+    
+/*
+const nombre = obtenerTextoValido("nombre");
+const apellido = obtenerTextoValido("apellido");
+const dni =  obtenerNumeroValido("documento");
+const edad = obtenerNumeroValido("edad");
+*/
